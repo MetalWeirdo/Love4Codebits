8567ee46fa3163c5ca0e465b0f41ff5933d6443e
Rafael Pato <xmetalweirdox@gmail.com>
2011-11-26 15:12:39 +0000

    Added the main activity, organized the code a LOT! It's not working because of a bug that I'm solving.

--- gen/info/love4codebits/app/R.java
+++ gen/info/love4codebits/app/R.java
@@ -15,15 +15,23 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btnLogin=0x7f050004;
+        public static final int btnCam=0x7f05000b;
+        public static final int btnChoose=0x7f05000c;
+        public static final int btnLogin=0x7f050005;
+        public static final int cbRemember=0x7f050004;
         public static final int etE_mail=0x7f050001;
         public static final int etPassword=0x7f050003;
-        public static final int progressBar1=0x7f050005;
+        public static final int ivAvatar=0x7f050008;
+        public static final int linearLayout1=0x7f050007;
+        public static final int progressBar1=0x7f050006;
         public static final int textView1=0x7f050000;
         public static final int textView2=0x7f050002;
+        public static final int tvNickname=0x7f050009;
+        public static final int tvUsername=0x7f05000a;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
