7000e6421936f4276ea0e05af6133fac1516da01
Rafael Pato <xmetalweirdox@gmail.com>
2011-11-27 12:19:02 +0000

    New Code,New View,Nem stuff

--- src/info/love4codebits/app/Main.java
+++ src/info/love4codebits/app/Main.java
@@ -4,17 +4,24 @@ import java.io.InputStream;
 import java.net.URL;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
-public class Main extends Activity{
+public class Main extends Activity implements OnClickListener{
 	
 	TextView tv1;
 	ImageView iv1;
+	Button btn1;
+	private static final int CAMERA_PIC_REQUEST = 4146;
 	
 	 public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
@@ -25,8 +32,24 @@ public class Main extends Activity{
 	        tv1.setText(LoadPreferences("nick"));
 	        tv1 = (TextView) findViewById(R.id.tvUsername);
 	        tv1.setText(LoadPreferences("name"));
+	        btn1 = (Button) findViewById(R.id.btnCam);
+	        btn1.setOnClickListener(this);
+	        
 	        
 	 }
+	public void onClick (View v){
+		 Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		 startActivityForResult(i, CAMERA_PIC_REQUEST);
+		 
+		 
+	 }
+	 protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+		    if (requestCode == CAMERA_PIC_REQUEST) {  
+		    	Bitmap thumbnail = (Bitmap) data.getExtras().get("data");  
+		    	
+		    	iv1.setImageBitmap(thumbnail);   
+		    }  
+		}  
 	 
 	 private String LoadPreferences(String key){
 	        SharedPreferences sharedPreferences = getSharedPreferences("LogIn",MODE_PRIVATE);
