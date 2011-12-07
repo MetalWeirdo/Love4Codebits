36035f81293d0a6c9974be81ea1cef2bf9f31070
Rafael Pato <xmetalweirdox@gmail.com>
2011-12-07 10:56:14 +0000

    Made a simple logo, but it's not a final logo! I don't have much of design skills... Still need to test the layout in smaller devices

--- gen/info/love4codebits/app/R.java
+++ gen/info/love4codebits/app/R.java
@@ -16,27 +16,29 @@ public final class R {
         public static final int armyofbots=0x7f020002;
         public static final int ic_launcher=0x7f020003;
         public static final int ic_menu_exit=0x7f020004;
+        public static final int logo=0x7f020005;
     }
     public static final class id {
-        public static final int about=0x7f060011;
-        public static final int btnCam=0x7f06000e;
-        public static final int btnChoose=0x7f06000f;
-        public static final int btnLogin=0x7f060009;
+        public static final int about=0x7f060012;
+        public static final int btnCam=0x7f06000f;
+        public static final int btnChoose=0x7f060010;
+        public static final int btnLogin=0x7f06000a;
         public static final int btnNo=0x7f060004;
         public static final int btnYes=0x7f060003;
-        public static final int cbRemember=0x7f060008;
-        public static final int etE_mail=0x7f060005;
-        public static final int etPassword=0x7f060007;
-        public static final int group1=0x7f060010;
-        public static final int ivAvatar=0x7f06000b;
+        public static final int cbRemember=0x7f060009;
+        public static final int etE_mail=0x7f060006;
+        public static final int etPassword=0x7f060008;
+        public static final int group1=0x7f060011;
+        public static final int imageView1=0x7f060005;
+        public static final int ivAvatar=0x7f06000c;
         public static final int ivPreview=0x7f060000;
         public static final int linearLayout1=0x7f060002;
-        public static final int logout=0x7f060012;
-        public static final int progressBar1=0x7f06000a;
+        public static final int logout=0x7f060013;
+        public static final int progressBar1=0x7f06000b;
         public static final int textView1=0x7f060001;
-        public static final int textView2=0x7f060006;
-        public static final int tvNickname=0x7f06000c;
-        public static final int tvUsername=0x7f06000d;
+        public static final int textView2=0x7f060007;
+        public static final int tvNickname=0x7f06000d;
+        public static final int tvUsername=0x7f06000e;
     }
     public static final class layout {
         public static final int imgpreview=0x7f030000;
